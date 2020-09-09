441ce8a2b2df7fbcada9cd1e5231e6c1718bed90
mrLuisFer <lolesuncrak@gmail.com>
2020-09-08 19:08:59 -0500

    Estructura del right dashboard

---
+++ src/components/TodayStatic.js
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react'
+
+import { WeatherContext } from '../context/WeatherContext'
+
+// Este componente ira a la derecha
+export default function CityTemp() {
+	const { weather, loading } = useContext(WeatherContext)
+
+	return (
+		<div>
+			{!loading && (
+				<div className='today'>
+					<h1 className='today_title'>Today</h1>
+					{/*TODO: Agregar icono o svg del clima <div> ICON </div> */}
+					<div className='today_date_container'>
+						<p className='today_date_number'>
+							{(weather.main.temp - 273.15).toFixed(2)}
+							<span className='today_date_number_span'>°C</span>
+						</p>
+						<p className='today_date_state'>{weather.weather[0].main}</p>
+					</div>
+				</div>
+			)}
+		</div>
+	)
+}
--- src/containers/RightDashboard.js
+++ src/containers/RightDashboard.js
@@ -2,18 +2,21 @@ import React from 'react'
 
 import Toogle from '../components/Toogle'
 import CityNameRight from '../components/CityNameRight'
+import TodayStatic from '../components/TodayStatic'
+import CardsRight from '../components/CardsRight'
 
 // Contenedor de la parte derecha de la app
 export default function RightDashboard() {
 	return (
-		<div className='rightDashboard'>
+		<section className='rightDashboard'>
+			{/* Switch Theme */}
 			<Toogle />
+			{/* Ciudad, pais, fecha con Date */}
 			<CityNameRight />
-			{
-				// ciudad, pais, fecha con Date
-				// today estatico deg  e icono del sol weather.weather[0].main
-				//cuadritos de viento, humedad, nubes = clouds.all
-			}
-		</div>
+			{/* Today estatico deg  e icono del sol weather.weather[0].main */}
+			<TodayStatic />
+			{/* Cuadritos de viento, humedad, nubes = clouds.all */}
+			<CardsRight />
+		</section>
 	)
 }
