c2c2ca1c5330ace306aabb232bf1f1a129bbc2f1
mrLuisFer <lolesuncrak@gmail.com>
2020-09-22 22:56:03 -0500

    Palabra theme añadida y que cambia dependiendo de la clase

--- src/components/Toogle.js
+++ src/components/Toogle.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Componente que cambiara el tema de la app
 export default function Toogle() {
+	const root = document.querySelector('#root')
+	const [theme, setTheme] = useState(false)
+
 	const handleChange = () => {
-		const root = document.querySelector('#root')
 		root.classList.toggle('dark')
+		if (root.classList.contains('dark')) {
+			setTheme(true)
+		} else {
+			setTheme(false)
+		}
 		console.log(root)
 	}
 
 	return (
 		<div className='toggle'>
+			{theme ? (
+				<span className='toggle_span_theme'>Ligth</span>
+			) : (
+				<span className='toggle_span_theme'>Dark</span>
+			)}
+			<span className='toggle_span'>Mode:</span>
 			<input className='toggle_input' type='checkbox' onChange={handleChange} />
 		</div>
 	)
